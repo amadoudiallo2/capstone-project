24a57a33dc08bd86b92c2e869cab2d90316c3197
Muheez Adedayo Opebiyi <105895941+ommaxyl@users.noreply.github.com>
2023-07-22 07:46:52 -0400

    Create nat.tf

---
+++ nat.tf
@@ -0,0 +1,25 @@
+# Create EIP for NAT GW1
+resource "aws_eip" "eip_gtw_1" {
+  count = "1"
+}
+
+# Create NAT gateway1
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  count         = "1"
+  allocation_id = aws_eip.eip_gtw_1[count.index].id
+  subnet_id     = aws_subnet.private_subnet2.id
+}
+
+# Create EIP for NAT GW2
+
+resource "aws_eip" "eip_gtw_2" {
+  count = "1"
+}
+
+# Create NAT gateway2
+
+resource "aws_nat_gateway" "nat_gateway_2" {
+  count         = "1"
+  allocation_id = aws_eip.eip_gtw_2[count.index].id
+  subnet_id     = aws_subnet.private_subnet3.id
