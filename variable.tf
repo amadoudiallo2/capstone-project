94068db5c40b0f17ff07a86b0e79402f29d07394
Muheez Adedayo Opebiyi <105895941+ommaxyl@users.noreply.github.com>
2023-07-22 07:36:43 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,46 @@
+variable "vpc-cidr-block" {
+  default = "10.0.0.0/16"
+}
+
+variable "public-sn-1-cidr_block" {
+  default = "10.0.1.0/24"
+}
+
+variable "private-sn-2-cidr_block" {
+  default = "10.0.2.0/24"
+}
+
+variable "private-sn-3-cidr_block" {
+  default = "10.0.3.0/24"
+}
+
+variable "private-sn-4-cidr_block" {
+  default = "10.0.4.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_value" {
+  default = "ami-06b09bfacae1453cb"
+}
+
+variable "key-name" {
+  default = "DemoKeyPair"
+}
+
+variable "availability_zone_1" {
+  default = "us-east-1a"
+}
+
+variable "availability_zone_2" {
+  default = "us-east-1b"
+}
+
+variable "availability_zone_3" {
+  default = "us-east-1c"
+}
+
+variable "bucketName" {
+  default = "my-static-s3-bucket-003"
